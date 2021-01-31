60aea487c07078d9659403415fca0f764279d3f3
George Thomas <georgefsthomas@gmail.com>
2021-01-31 13:15:46 +0000

    Miscellaneous improvements
    This essentially cherry picks from the 'hls' branch, which is now pretty useless since that logo didn't win.

--- logo.hs
+++ logo.hs
@@ -1,75 +1,98 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# OPTIONS_GHC -Wall #-}
 
-module Main (main) where
+module Main where
 
+import Data.ByteString.Lazy.Char8 qualified as BSL
+import Data.Text.Encoding (decodeUtf8)
 import Diagrams.Backend.SVG
-import Diagrams.Backend.SVG.CmdLine
 import Diagrams.Prelude
+import Graphics.Svg (renderBS)
+import Svgone qualified
 
 {- TODO
 turn in to cabal script
     for now:
-        cabal install --package-env . --allow-newer='*:base' --lib diagrams-lib diagrams-core diagrams-svg
-upload to wiki
-    include original, simpler, explicit points version
+        cabal install --package-env . --allow-newer='*:base' --lib \
+            diagrams-lib diagrams-core diagrams-svg svg-builder svgone
+less use of absolute positions
 -}
 
 main :: IO ()
-main = mainWith $ d & center & pad 1.1 & lwG 0.5
+main = do
+    let d = renderBS . renderDia SVG opts $ hs & center & scaleY 1.5 & pad 1.1 & lw 0
+    BSL.writeFile "out-raw.svg" d
+    Svgone.run Svgone.allPluginsWithDefaults "" (decodeUtf8 $ BSL.toStrict d) "out.svg"
+  where
+    opts =
+        SVGOptions
+            { _size = mkSizeSpec $ V2 (Just 1000) Nothing
+            , _svgDefinitions = Nothing
+            , _idPrefix = ""
+            , _svgAttributes = []
+            , _generateDoctype = True
+            }
 
-d :: Diagram B
-d =
+hs :: Diagram B
+hs =
     position
         [
             ( p2 (0, 0)
-            , reflectX (diagonal purple0)
-                === reflectX (reflectY (diagonal purple0))
+            , reflectX (diagonal 120) === reflectX (reflectY (diagonal 120))
+                & fc purple0
             )
         ,
             ( p2 (120, 0)
-            , reflectX (diagonal purple1)
-                === ( reflectY (diagonal purple1)
-                        <> reflectX (reflectY (diagonal purple1))
+            , reflectX (diagonal 120)
+                === ( (diagonal 120 & reflectY & reflectX)
+                        <> (diagonal 120 & reflectY & snugR)
                     )
+                & fc purple1
             )
         ,
-            ( p2 (145, 75)
-            , reflectY $ horizontal purple2 200
+            ( p2 (100, 50)
+            , reflectY (horizontalChopped 200)
+                & fc purple2
             )
         ,
-            ( p2 (205, -15)
-            , reflectY $ horizontal purple2 140
+            ( p2 (160, -10)
+            , reflectY (horizontalChopped 140)
+                & fc purple2
             )
         ]
 
-diagonal :: Colour Double -> Diagram B
-diagonal c =
-    translate (V2 (-45) 0) $
-        polygonFromCoords
-            c
-            [ (0, 0)
-            , (120, 180)
-            , (210, 180)
-            , (90, 0)
-            ]
-
-horizontal :: Colour Double -> Double -> Diagram B
-horizontal c x =
+diagonal :: Double -> Diagram B
+diagonal y =
     polygonFromCoords
-        c
         [ (0, 0)
-        , (40, 60)
-        , (x, 60)
+        , (y, y)
+        , (y + 90, y)
+        , (90, 0)
+        ]
+
+horizontalChopped :: Double -> Diagram B
+horizontalChopped x =
+    polygonFromCoords
+        [ (0, 0)
+        , (40, 40)
+        , (x, 40)
         , (x, 0)
         ]
 
-polygonFromCoords :: Colour Double -> [(Double, Double)] -> Diagram B
-polygonFromCoords c =
-    fc c
-        . lc c
-        . stroke
+horizontal :: Double -> Diagram B
+horizontal x =
+    polygonFromCoords
+        [ (0, 0)
+        , (40, 40)
+        , (x + 40, 40)
+        , (x, 0)
+        ]
+
+polygonFromCoords :: [(Double, Double)] -> Diagram B
+polygonFromCoords =
+    stroke
         . closeLine
         . fromVertices
         . map p2
