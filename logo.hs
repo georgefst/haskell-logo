e72a7af0b5435153b5a8e01ca63a74a30e7235cd
George Thomas <georgefsthomas@gmail.com>
2024-10-25 18:12:14 +0100

    Scale down tick dimensions
    The new checkbox width has been chosen so that the borders match the tick width. We might alternatively want them to match the vertical borders (unless they somehow are already, via some maths that I've missed), or even choose a scale factor other than 55/90 such that both are true simultaneously.

--- logo.hs
+++ logo.hs
@@ -65,18 +65,20 @@ hs =
                 )
             & fc purple1
             & skew
-        , mconcat
-            [ ( (diagonal 120 & translateY -30)
-                    <> (diagonal 60 & centerY & reflectX)
-              )
-                & translateX -85
-                & fc purple0
-                & skew
-            , reflectY (horizontalChopped 250)
-                & fc purple2
-                & skew
-            ]
-            & translateX 200 -- TODO something more principled (`snugL` should work but envelope isn't tight enough)
+        , let sf = 55 / 90 -- intended to look consistent with other line widths of 35 and 90
+           in mconcat
+                [ ( (diagonal 180 & translateY (-75 / 2))
+                        <> (diagonal 75 & centerY & reflectX)
+                  )
+                    & translateX -135
+                    & scale sf
+                    & fc purple0
+                    & skew
+                , reflectY (horizontalChopped (390 * sf))
+                    & fc purple2
+                    & skew
+                ]
+                & translateX (390 * sf - 50) -- TODO something more principled (`snugL` should work but envelope isn't tight enough)
         ]
   where
     skew = scaleY 1.5
